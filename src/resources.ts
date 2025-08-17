d289ff51d96d6b8aa4c76b97b214bf4e0f31dec9
Andrei Marukovich <marukovich@gmail.com>
2025-08-17 15:48:40 -0400

    Initial site implementation

---
+++ src/resources.ts
@@ -0,0 +1,7 @@
+export const SITE_TITLE = 'Atólena';
+
+export const NAV_LINKS = [
+  { href: '/', label: 'Home' },
+  { href: '/about', label: 'About' },
+  { href: '/contact', label: 'Contact' }
+]
